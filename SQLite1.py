3e60c68831bb35198662d956fb72484d4b6a3523
shreesh123 <shreeshsrvstv@gmail.com>
2020-07-31 19:10:21 +0530

    Adding the comments

--- SQLite1.py
+++ SQLite1.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+#Creating the SQLite file
 conn = sqlite3.connect('emaildb.sqlite')
 cur = conn.cursor()
 
@@ -11,13 +12,20 @@ CREATE TABLE Counts (org TEXT, count INTEGER)''')
 fname = input('Enter file name: ')
 if (len(fname) < 1): fname = 'mbox-short.txt'
 fh = open(fname)
-count = 0
+# count = 0
 for line in fh:
+
+    #Creating the list of all the files starting from 'From: '
     if not line.startswith('From: '): continue
     email = line.split('@')
-    count = count + 1
-    print(count)
-    print(email[1])
+
+
+    #for checking the values
+    # count = count + 1
+    # print(count)
+    # print(email[1])
+
+    #Condition
     cur.execute('SELECT count FROM Counts WHERE org = ? ', (email[1],))
     row = cur.fetchone()
     if row is None:
@@ -28,7 +36,7 @@ for line in fh:
                     (email[1],))
     conn.commit()
 
-# https://www.sqlite.org/lang_select.html
+# Printing the values
 sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 
 for row in cur.execute(sqlstr):
